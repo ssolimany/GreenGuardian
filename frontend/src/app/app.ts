53712a5f7828f629051b30d632fcea5c9fb8530d
Saman Solimany <saman@solimany.com>
2025-06-28 18:56:50 +0200

    Add grid material ui component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatIconModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected title = 'frontend';
-}
+export class App {}
---
+++ frontend/src/app/home-page/home-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+@Component({
+  selector: 'app-home-page',
+  imports: [MatGridListModule],
+  templateUrl: './home-page.html',
+  styleUrl: './home-page.css'
+})
+export class HomePage {
+
+}
